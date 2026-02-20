d1f7c6752bba6db5c0dd7560fd64fc4b6c798c2e
James Calderon <j.e.calderon@gmail.com>
2026-02-20 12:20:52 -0500

    Add RPM Fusion repository installation to build script

--- build_files/build.sh
+++ build_files/build.sh
@@ -34,6 +34,11 @@ set -ouex pipefail
 
 ### DIRECT REPO INSTALLS
 
+# RPM Fusion repos (free and nonfree)
+    dnf5 -y install \
+        https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
+        https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm
+
 # vscode repo
     curl -o /etc/yum.repos.d/vscode.repo "https://packages.microsoft.com/yumrepos/vscode/config.repo"
 
