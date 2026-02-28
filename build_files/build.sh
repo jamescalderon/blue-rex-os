a2e30039fee68c6ff2d58a3e9b9890ab13dae448
James Calderon <j.e.calderon@gmail.com>
2026-02-28 07:00:28 -0500

    Add Spotify client to package installation in build script

--- build_files/build.sh
+++ build_files/build.sh
@@ -93,12 +93,13 @@ safeInstall() {
         obs-studio \
         obs-studio-plugin-pwvideo 
 
-# Web Browsers & Apps
+# Apps
     safeInstall \
         firefox \
         thunderbird \
         webapp-manager \
-        chromium
+        chromium \
+        spotify-client
 
 # Firefox PWA (not available in repos, so we install it directly from the RPM)
     curl -Lo /tmp/firefoxpwa.rpm https://github.com/filips123/PWAsForFirefox/releases/download/v2.18.0/firefoxpwa-2.18.0-1.x86_64.rpm
